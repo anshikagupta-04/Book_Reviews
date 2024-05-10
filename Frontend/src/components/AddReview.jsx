d91f09619daa1ae4d384019225a23020d6d05d35
anshikagupta-04 <pg8998211@gmail.com>
2024-05-10 16:34:18 +0530

    review add functionality

--- Frontend/src/components/AddReview.jsx
+++ Frontend/src/components/AddReview.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import Reviews from "./Reviews"
+import axios from "axios";
+import { useState } from "react";
 
 const AddReview = () => {
-  return (
-    <div>AddReview</div>
-  )
+    const [reviews, setReview] = useState([]);
+    const [createForm, setCreateForm] = useState({
+        bookTitle: "",
+        author: "",
+        review: "",
+        by: "",
+      });
+      const [updateForm, setUpdateForm] = useState({
+        _id: null,
+        bookTitle: "",
+        author: "",
+        review: "",
+        by: "",
+      });
+
+    const updateCreateFormField = async (e) => {
+        const { name, value } = e.target;
+    
+        await setCreateForm({
+          ...createForm,
+          [name]: value,
+        });
+      };
+    
+      const createReview = async (e) => {
+        e.preventDefault();
+        try{
+        //create review
+        const res = await axios.post("/api/review",  createForm);
+    
+        // update state
+        setReview([...reviews, res.data.review]);
+    
+        //reset from
+        setCreateForm({
+          bookTitle: "",
+          author: "",
+          review: "",
+          by: ""
+        });
+    }catch(error){
+        console.log(error);
+    }
+      };
+
+    return (
+        <div>
+            <div>
+                <h2>Update note</h2>
+                <form onSubmit={createReview}>
+                    <input
+                        onChange={updateCreateFormField}
+                        value={updateForm.title}
+                        name="bookTitle"
+                    />
+                    <input
+                        onChange={updateCreateFormField}
+                        value={updateForm.title}
+                        name="author"
+                    />
+                    <textarea
+                        onChange={updateCreateFormField}
+                        value={updateForm.body}
+                        name="review"
+                    />
+                    <input
+                        onChange={updateCreateFormField}
+                        value={updateForm.title}
+                        name="by"
+                    />
+                    <button type="submit">Update note</button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default AddReview
--- Frontend/src/components/Reviews.jsx
+++ Frontend/src/components/Reviews.jsx
@@ -3,23 +3,58 @@ import axios from "axios";
 
 const Reviews = () => {
   const [reviews, setReview] = useState([]);
+  const [createForm, setCreateForm] = useState({
+    bookTitle: "",
+    author: "",
+    review: "",
+    by: "",
+  });
+  const [updateForm, setUpdateForm] = useState({
+    _id: null,
+    bookTitle: "",
+    author: "",
+    review: "",
+    by: "",
+  });
 
   useEffect(() => {
     fetchReviews();
   }, []);
 
-  const fetchReviews = async () => {
-    // Fetch the notes
-    // const res = 
+  const fetchReviews = async () => { 
     await axios.get("/api/review").then(function (response) {
       setReview(response.data.review);
     })
     .catch(function (error) {
       console.log(error);
     })
+  };
 
-    // Set to state
-    // setReview(res.data.reviews);
+  const updateCreateFormField = async (e) => {
+    const { name, value } = e.target;
+
+    await setCreateForm({
+      ...createForm,
+      [name]: value,
+    });
+  };
+
+  const createReview = async (e) => {
+    e.preventDefault();
+
+    //create review
+    const res = await axios.post("/api/review",  createForm);
+
+    // update state
+    setReview([...reviews, res.data.review]);
+
+    //reset from
+    setCreateForm({
+      bookTitle: "",
+      author: "",
+      review: "",
+      by: ""
+    });
   };
 
   return (
@@ -38,6 +73,34 @@ const Reviews = () => {
             );
           })}
       </div>
+      {/* {updateForm._id && ( */}
+        <div>
+          <h2>Update note</h2>
+          <form onSubmit={createReview}>
+            <input
+              onChange={updateCreateFormField}
+              value={updateForm.title}
+              name="bookTitle"
+            />
+            <input 
+            onChange={updateCreateFormField}
+            value={updateForm.title}
+            name="author"
+            />
+            <textarea
+              onChange={updateCreateFormField}
+              value={updateForm.body}
+              name="review"
+            />
+            <input 
+            onChange={updateCreateFormField}
+              value={updateForm.title} 
+              name="by"
+              />
+            <button type="submit">Update note</button>
+          </form>
+        </div>
+      {/* )} */}
       </div>
   );
 }
